f5e0d82cc50d736a4e960f36b649e86f7a78eb7c
Fernando <fernando@park-it-solutions.com>
2018-11-07 21:48:02 +0000

    Add draggable to workerCard component

--- src/components/WorkerCard/WorkerCard.js
+++ src/components/WorkerCard/WorkerCard.js
@@ -1,21 +1,46 @@
-import React from 'react'
-import { FaCheckCircle, FaTimesCircle } from 'react-icons/fa';
+import React, { Component } from 'react'
+import { FaCheckCircle, FaTimesCircle } from 'react-icons/fa'
+import Draggable from 'react-draggable'
 import WorkerInfo from '../WorkerInfo'
 import styles from './styles'
-const WorkerCard = (props) => {
-  return (
-    <div>
-      <WorkerInfo
-        imageUrl="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg"
-        firstName="Fer"
-        lastName="Calvo"
-      />
-      <div style={styles.buttons}>
-        <FaCheckCircle style={styles.iconApprove}/>
-        <FaTimesCircle style={styles.iconReject} />
+
+class WorkerCard extends Component {
+  handleStop = (e, data) => {
+    if (data.lastX < -100) {
+      console.log('reject')
+    } else if (data.lastX > 100) {
+      console.log('approve')
+    }
+  }
+  render() {
+    return (
+      <div style={{
+        width: 500
+      }}>
+        <Draggable
+          axis="x"
+          defaultPosition={{x: 0, y: 0}}
+          onStop={this.handleStop}
+          position={{
+            x: 0,
+            y: 0
+          }}
+        >
+          <div>
+          <WorkerInfo
+            imageUrl="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg"
+            firstName="Fer"
+            lastName="Calvo"
+          />
+          </div>
+        </Draggable>
+        <div style={styles.buttons}>
+          <FaCheckCircle style={styles.iconApprove}/>
+          <FaTimesCircle style={styles.iconReject} />
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default WorkerCard
--- src/components/WorkerInfo/WorkerInfo.js
+++ src/components/WorkerInfo/WorkerInfo.js
@@ -7,6 +7,7 @@ const WorkerInfo = (props) => {
   return (
     <div style={styles.container}>
       <img
+      draggable="false"
         style={styles.image}
         src={imageUrl}
         alt={`${firstName} ${lastName}`}
