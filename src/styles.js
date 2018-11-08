ad8ffd97bf23d50a9a6a1f713b69c6376bafa791
Fernando <fernando@park-it-solutions.com>
2018-11-08 01:36:35 +0000

    Display everything in App

---
+++ src/styles.js
@@ -0,0 +1,13 @@
+export default {
+  wrapper: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh'
+  },
+  notification: {
+    position: 'fixed',
+    top: 0,
+    width: '100%'
+  }
+}
