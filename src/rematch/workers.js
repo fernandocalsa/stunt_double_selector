5fe108f3996dfe13819b3585c815131035491267
Fernando <fernando@park-it-solutions.com>
2018-11-08 01:08:23 +0000

    Load actors from themoviedb

--- src/rematch/workers.js
+++ src/rematch/workers.js
@@ -1,18 +1,9 @@
+import uniqid from 'uniqid'
+
+const apiKey = process.env.REACT_APP_API_KEY
+
 export default {
-  state: [
-    {
-      id: 1,
-      imageUrl: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg",
-      firstName: "fer",
-      lastName: "calvo"
-    },
-    {
-      id: 2,
-      imageUrl: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg",
-      firstName: "FER",
-      lastName: "CALVO"
-    }
-  ],
+  state: [],
   reducers: {
     updateApprovedStatus(state, { workerId, approved }) {
       const workerIndex = state.findIndex(({ id }) => id === workerId)
@@ -25,6 +16,12 @@ export default {
         approved
       }
       return all
+    },
+    add(state, worker) {
+      return [
+        ...state,
+        worker
+      ]
     }
   },
   effects: (dispatch) => ({
@@ -33,6 +30,23 @@ export default {
     },
     reject(workerId) {
       dispatch.workers.updateApprovedStatus({ workerId, approved: false})
+    },
+    async load() {
+      const page = Math.floor(Math.random() * 100)
+      const url = `https://api.themoviedb.org/3/search/person?query=a&page=${page}&api_key=${apiKey}`
+      const response = await fetch(url)
+      const body = await response.json()
+      const people = body.results
+        .filter(({ profile_path }) => !!profile_path)
+        .slice(0, 10)
+      if (!people.length) {
+        return dispatch.workers.load()
+      }
+      people.map(actor => dispatch.workers.add({
+        imageUrl: 'https://image.tmdb.org/t/p/w300' + actor.profile_path,
+        name: actor.name,
+        id: uniqid()
+      }))
     }
   })
 }
